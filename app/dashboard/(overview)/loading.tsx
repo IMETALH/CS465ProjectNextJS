b0f57f340d914f05348711f8445a4b1314394570
keny kit <imetalh@Imetal.local>
2025-01-01 21:39:10 -0500

    static and dynamic redering, and beginning streaming

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,10 @@
+import DashboardSkeleton from "@/app/ui/skeletons";
+
+export default function Loading() {
+  return (
+    <div>
+      <p>Loading...</p>
+      <DashboardSkeleton />
+    </div>
+  );
+}
